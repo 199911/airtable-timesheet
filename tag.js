0be8ee01fc847275b3913c64b49a44284389d20d
Sunday <sunday@stepcase.com>
2017-06-25 22:10:00 +0800

    Add logic to allow short form of mood and difficulty

---
+++ tag.js
@@ -0,0 +1,29 @@
+const config = require('./config.json');
+
+
+const normalize = (type, word) => {
+    let tags = [];
+    if (type == 'Mood') {
+        tags = [
+            "???",
+            "...",
+            "Fuck",
+            "Angry",
+            "OK"
+        ];
+    } else if (type == 'Difficulty'){
+        tags = [
+            "Very Easy",
+            "OK",
+            "Hard",
+            "Fuck",
+            "Easy"
+        ];
+    }
+    // Match the first letter, return the original word if not found
+    return tags.find((tag) => (tag.toLowerCase()[0] === word.toLowerCase()[0])) || word;
+}
+
+module.exports = {
+    normalize
+};
--- timesheet.js
+++ timesheet.js
@@ -76,4 +76,4 @@ module.exports = {
     startAsync,
     endAsync,
     listAsync
-}
+};
