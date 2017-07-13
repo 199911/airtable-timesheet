bb94fa49cd69ca3dcc9b5f42b98c5eda8f4ff0ec
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-07-13 13:19:55 +0800

    Clean up code
    - Refactor tag.js
    - Add example base to Read me

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A simple cli command to record task on airtable
 
 ## Set up
 
-1. Create an base on airtable
-2. Rename table to ddMmm, like 25Jun
+1. Copy the base from example
+    - https://airtable.com/invite/l?inviteId=inv9sQOYI2AhSH0gv&inviteToken=3394597e3997ea0c67f85c2f65946dec
+2. Duplicate a table and rename to format `ddMmm`, like `25Jun`
 3. Set up columns
 	1.  ID: auto number
 	2.  Description: long text
@@ -15,6 +16,8 @@ A simple cli command to record task on airtable
 	6.  End: Date
 4. Set up api key and base in config.json
 5. Set up the cli (TODO: add more detail)
+    - `npm install`
+    - `ln -s $(pwd)/bin /usr/local/bin/t`
 
 ## Todo
 
--- tag.js
+++ tag.js
@@ -1,26 +1,15 @@
 const config = require('./config.json');
 
 
-const normalize = (type, word) => {
-    let tags = [];
-    if (type == 'Mood') {
-        tags = [
-            "???",
-            "...",
-            "Fuck",
-            "Angry",
-            "OK",
-            "Nice"
-        ];
-    } else if (type == 'Difficulty'){
-        tags = [
-            "Very Easy",
-            "OK",
-            "Hard",
-            "Fuck",
-            "Easy"
-        ];
-    }
+const normalize = (word) => {
+    const tags = [
+        "???",
+        "...",
+        "Fuck",
+        "Angry",
+        "OK",
+        "Nice"
+    ];
     // Match the first letter, return the original word if not found
     return tags.find((tag) => (tag.toLowerCase()[0] === word.toLowerCase()[0])) || word;
 }
